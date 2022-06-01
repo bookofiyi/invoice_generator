bfa612c9b9fe0dc96abb82439ea2f63a4fc04fd6
Iyiola Edwin Adedeji <iyiolaeadedeji@gmail.com>
2022-06-01 15:33:08 +0100

    finalized onboarding (boilerplate) screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:invoice_generator/constants.dart';
 import 'package:invoice_generator/form.dart';
+import 'package:invoice_generator/onboarding/onboarding_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+  final showHome = prefs.getBool('showHome') ?? false;
+  runApp(MyApp(showHome: showHome));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final bool? showHome;
+  const MyApp({
+    Key? key,
+    required this.showHome,
+  }) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -42,7 +52,7 @@ class MyApp extends StatelessWidget {
       //   ),
       // colorScheme:
       //     ColorScheme.fromSwatch().copyWith(secondary: Colors.redAccent),
-      home: const FormScreen(),
+      home: showHome! ? const FormScreen() : const OnboardingScreen(),
     );
   }
 }
--- lib/onboarding/onboarding_screen.dart
+++ lib/onboarding/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:invoice_generator/form.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -10,6 +12,7 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   final controller = PageController();
+  bool isLastPage = false;
 
   @override
   void dispose() {
@@ -23,56 +26,140 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       body: Container(
         padding: const EdgeInsets.only(bottom: 80),
         child: PageView(
-          controller: controller,
+          controller: controller, // allows working with the PageView
+          onPageChanged: (index) {
+            setState(() {
+              isLastPage = index == 2;
+            });
+          },
           children: [
-            Container(
+            // creates a container that holds the info for each slide
+            buildPage(
               color: Colors.red,
-              child: const Center(
-                child: Text('Page 1'),
-              ),
+              urlImage: 'assets/images/logo.png',
+              title: 'TITLE HERE',
+              subtitle: 'Your subtitle here',
             ),
-            Container(
-              color: Colors.indigo,
-              child: const Center(
-                child: Text('Page 2'),
-              ),
+            buildPage(
+              color: Colors.red,
+              urlImage: 'assets/images/logo.png',
+              title: 'TITLE HERE',
+              subtitle: 'Your subtitle here',
             ),
-            Container(
-              color: Colors.green,
-              child: const Center(
-                child: Text('Page 3'),
-              ),
+            buildPage(
+              color: Colors.red,
+              urlImage: 'assets/images/logo.png',
+              title: 'TITLE HERE',
+              subtitle: 'Your subtitle here',
             ),
           ],
         ),
       ),
-      bottomSheet: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        height: 80,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            TextButton(
-              child: const Text('SKIP'),
-              onPressed: () {},
-            ),
-            Center(
-              child: SmoothPageIndicator(
-                controller: controller,
-                count: 3,
-                effect: const WormEffect(
-                  spacing: 16,
-                  dotColor: Colors.black26,
-                  activeDotColor: Colors.red,
+      bottomSheet: isLastPage
+          ? TextButton(
+              onPressed: () async {
+                // navigates to home page and saves that onboarding has been viewed
+                final prefs = await SharedPreferences.getInstance();
+                prefs.setBool('showHome', true);
+
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const FormScreen()));
+              },
+              child: const Text(
+                'Get Started',
+                style: TextStyle(
+                  fontSize: 24,
                 ),
               ),
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                primary: Colors.white,
+                backgroundColor: Colors.red,
+                minimumSize: const Size.fromHeight(80),
+              ),
+            )
+          : Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              height: 80,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    child: const Text('SKIP'),
+                    onPressed: () => controller.jumpToPage(2),
+                  ),
+                  Center(
+                    child: SmoothPageIndicator(
+                      controller: controller,
+                      count: 3,
+                      onDotClicked: (index) => controller.animateToPage(
+                        index,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeIn,
+                      ),
+                      effect: const WormEffect(
+                        spacing: 16,
+                        dotColor: Colors.black26,
+                        activeDotColor: Colors.red,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    child: const Text('NEXT'),
+                    onPressed: () => controller.nextPage(
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            TextButton(
-              child: const Text('NEXT'),
-              onPressed: () {},
+    );
+  }
+
+  Widget buildPage({
+    required Color color,
+    required String urlImage,
+    required String title,
+    required String subtitle,
+  }) {
+    return Container(
+      color: color,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            urlImage,
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+          const SizedBox(
+            height: 64,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
             ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              subtitle,
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
