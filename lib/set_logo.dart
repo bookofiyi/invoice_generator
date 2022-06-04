de8bc7bb568d1ec1c75c7663554399ab816f8201
Iyiola Edwin Adedeji <iyiolaeadedeji@gmail.com>
2022-06-04 23:28:29 +0100

    added image picking logic

--- lib/set_logo.dart
+++ lib/set_logo.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:invoice_generator/constants.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class SetLogoScreen extends StatefulWidget {
   const SetLogoScreen({Key? key}) : super(key: key);
@@ -9,6 +11,36 @@ class SetLogoScreen extends StatefulWidget {
 }
 
 class _SetLogoScreenState extends State<SetLogoScreen> {
+  XFile? pickedFile;
+  File? newImage;
+  String? path;
+
+  getImageFromGallery() async {
+    pickedFile = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 200,
+      maxWidth: 200,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        newImage = File(pickedFile!.path);
+      });
+    }
+  }
+
+  // Widget imageFromGallery() {
+  //   return FutureBuilder<File>(
+  //     builder: (BuildContext context, AsyncSnapshot<File?> snapshot) {
+  //       if (snapshot.connectionState == ConnectionState.done) {
+  //         if (snapshot.data == null) {
+  //           return const Text("Error loading file");
+  //         }
+  //       }
+  //         return Image.file(imageFile!);
+  //     },
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,11 +57,16 @@ class _SetLogoScreenState extends State<SetLogoScreen> {
         child: Column(
           children: [
             Center(
-              child: Image.asset(
-                'assets/images/logo.png',
-                width: 200,
-                height: 200,
-              ),
+              child: newImage == null
+                  ? Image.asset(
+                      'assets/images/logo.png',
+                      width: 200,
+                      height: 200,
+                    )
+                  : Image.file(
+                      newImage!,
+                      fit: BoxFit.cover,
+                    ),
             ),
             const SizedBox(
               height: 20,
